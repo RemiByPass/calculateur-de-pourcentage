01a326c73bc9f29419e36ab7a18c953ce643eaea
RemiByPass <49651279+RemiByPass@users.noreply.github.com>
2020-10-15 18:34:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import time
 print("Codez par Remi_ByPass#7867")
-print("Valeur du prdouit (en €)")
+print("Valeur du produit (en €)")
 prix = float(input("Le produit vaut : "))
+time.sleep(1)
 print("Donnez la remise en % \nExemple: 40% de remise sur un produit d'une valeur de 48€ (99 au max) :")
 remise = float(input())
 while remise >=100:
@@ -9,6 +10,8 @@ while remise >=100:
     remise = float(input('Remise = '))
 total = prix*remise
 prix_total = total/100-prix
+time.sleep(1)
 print("Le produit (",prix,"€ ) coutera", -prix_total,"€ avec une remise de", remise, "%")
+time.sleep(1)
 print("Mon discord: https://discord.gg/vEz64HF")
 time.sleep(5)
